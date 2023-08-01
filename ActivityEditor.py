90fa0e9ac8167daad28c05207cf17e1013d2a0d7
smukhyala <smukhyala@gmail.com>
2023-08-01 12:28:22 -0700

    working on json stuff

--- ActivityEditor.py
+++ ActivityEditor.py
@@ -38,17 +38,28 @@ class Maker(ft.UserControl):
             mainData = dataDict
             return mainData
 
+        #check val
+        def is_number(value):
+            try:
+                float(value)
+                return True
+            except ValueError:
+                return False
+
         EAval = existingActivities()
+        data = EAval
 
         self.displayText = ft.Text('Create and activity below...', color=ft.colors.BLACK, size=25, height=60)
-        self.buffer = ft.Container(height = 20)
+        self.buffer = ft.Container(height = 14)
 
         #Temp
         self.TempIcon = ft.IconButton(icon=ft.icons.DEVICE_THERMOSTAT, 
                                     icon_color=ft.colors.RED, icon_size=60,
                                     tooltip="Temperature")
-        self.TempHi = ft.TextField(label="Highest Preferred Temperature", width = 200)
-        self.TempLo = ft.TextField(label="Lowest Preferred Temperature")
+        self.TempHi = ft.TextField(label="Highest Preferred Temperature", 
+                                    width = 250)
+        self.TempLo = ft.TextField(label="Lowest Preferred Temperature", 
+                                    width = 250)
         self.TempFieldCol = ft.Column(controls=[])
         self.TempFieldCol.controls.append(self.TempHi)
         self.TempFieldCol.controls.append(self.TempLo)
@@ -60,8 +71,10 @@ class Maker(ft.UserControl):
         self.WindIcon = ft.IconButton(icon=ft.icons.WIND_POWER, 
                                     icon_color=ft.colors.BLUE, icon_size=60,
                                     tooltip="Wind")
-        self.WindHi = ft.TextField(label="Highest Preferred Wind Speed")
-        self.WindLo = ft.TextField(label="Lowest Preferred Wind Speed")
+        self.WindHi = ft.TextField(label="Highest Preferred Wind Speed", 
+                                    width = 250)
+        self.WindLo = ft.TextField(label="Lowest Preferred Wind Speed", 
+                                    width = 250)
         self.WindFieldCol = ft.Column(controls=[])
         self.WindFieldCol.controls.append(self.WindHi)
         self.WindFieldCol.controls.append(self.WindLo)
@@ -73,8 +86,10 @@ class Maker(ft.UserControl):
         self.HumidityIcon = ft.IconButton(icon=ft.icons.WAVES, 
                                     icon_color=ft.colors.ORANGE, icon_size=60,
                                     tooltip="Humidity")
-        self.HumidityHi = ft.TextField(label="Highest Preferred Humidity")
-        self.HumidityLo = ft.TextField(label="Lowest Preferred Humidity")
+        self.HumidityHi = ft.TextField(label="Highest Preferred Humidity", 
+                                    width = 250)
+        self.HumidityLo = ft.TextField(label="Lowest Preferred Humidity", 
+                                    width = 250)
         self.HumidityFieldCol = ft.Column(controls=[])
         self.HumidityFieldCol.controls.append(self.HumidityHi)
         self.HumidityFieldCol.controls.append(self.HumidityLo)
@@ -86,14 +101,40 @@ class Maker(ft.UserControl):
         self.InfoIcon = ft.IconButton(icon=ft.icons.PLACE, 
                                     icon_color=ft.colors.GREEN, icon_size=60,
                                     tooltip="Name & City")
-        self.Name = ft.TextField(label="Activity Name")
-        self.City = ft.TextField(label="Activity Location (City)")
+        self.Name = ft.TextField(label="Activity Name", 
+                                    width = 250)
+        self.City = ft.TextField(label="Activity Location (City)", 
+                                    width = 250)
         self.InfoFieldCol = ft.Column(controls=[])
         self.InfoFieldCol.controls.append(self.Name)
         self.InfoFieldCol.controls.append(self.City)
         self.InfoRow = ft.Row(controls=[])
         self.InfoRow.controls.append(self.InfoIcon)
         self.InfoRow.controls.append(self.InfoFieldCol)
+
+        #add to json
+        user_data = {
+        "title": "",
+        "subtitle": "",
+        "icon": "",
+        "HighTemp": 0,
+        "LowTemp": 0,
+        "HighWind": 0,
+        "LowWind": 0,
+        "HighHumidity": 0,
+        "LowHumidity": 0,
+        "ActivityChoice": "nothing",
+        "CityChoice": "San Francisco"}
+
+        def sendToJson(self):
+            if not all(is_number(val) for val in [user_data]):
+                print("Please enter valid numbers for temperature, wind, and humidity.")
+                return
+            else:
+                return
+
+        self.submitButton = ft.FloatingActionButton(bottom=2,right=20, bgcolor = ft.colors.BLACK,
+                        on_click=sendToJson, icon = ft.icons.LIBRARY_ADD_CHECK_ROUNDED, text = "Submit")
 
         #define items in container
         Container = ft.Container(
