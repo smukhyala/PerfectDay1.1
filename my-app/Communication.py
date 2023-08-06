6a5125ebee2bc564c39537257b0661646f70cb94
smukhyala <smukhyala@gmail.com>
2023-08-06 08:37:26 -0700

    import class error

--- my-app/Communication.py
+++ my-app/Communication.py
@@ -9,12 +9,54 @@ from os.path import exists
 #flet imports
 import flet as ft
 
-class Editor(ft.UserControl):
+class Preferences(ft.UserControl):
 
     def build(self):
-        Container = ft.Container()
-
+       
         #set items
-        self.fieldCol = ft.Column(scroll = 'auto', height = 400, controls = [])
+        self.title = ft.Text("Communication Preferences", color = ft.colors.BLACK, size = 20)
+        self.buffer = ft.Container(height = 1)
+
+        self.email = ft.Text("Enter your email...", color = ft.colors.GREY_900, size = 15)
+        self.emailField = ft.TextField(label="janedoe@gmail.com", 
+                                    width = 120,
+                                    height = 35,
+                                    border_color = ft.colors.BLACK,
+                                    bgcolor = ft.colors.GREY_100,
+                                    color = ft.colors.BLACK)
+
+        self.time = ft.TextField("How often do you want PerfectDay updates?", 
+            color = ft.colors.GREY_900, size = 15)
+        self.timeField = ft.RadioGroup(content=ft.Column([
+            ft.Radio(value="daily", label="Daily"),
+            ft.Radio(value="biweekly", label="Twice Weekly"),
+            ft.Radio(value="weekly", label="Weekly")]))
+        
+        self.dd = ft.Dropdown(
+            width=100,
+            options=[
+                ft.dropdown.Option("Red"),
+                ft.dropdown.Option("Green"),
+                ft.dropdown.Option("Blue"),
+            ],
+        )
+
+        self.fieldCol = ft.Column(scroll = 'auto', height = 400, controls = [
+            self.email,
+            self.emailField,
+            self.buffer,
+            self.time,
+            self.timeField,
+            self.dd,
+        ])
+
+        Container = ft.Container(
+             content=ft.Column(
+            controls=[
+                self.title,
+                self.fieldCol
+            ]
+            )
+        )
 
         return Container
